62cec2ae2462330c727077d09f9f89f8655a5f4e
Beetrootmonkey <n.adelhard@googlemail.com>
2018-03-06 23:39:59 +0100

    Initial setup, pt. 2
    Finished implementing my old custom burnable items and edible food that gives potions effects. Plus I managed to get the loading of the config file AND ingame editing working.

--- src/main/java/com/beetrootmonkey/myutils/MyUtils.java
+++ src/main/java/com/beetrootmonkey/myutils/MyUtils.java
@@ -2,16 +2,22 @@ package com.beetrootmonkey.myutils;
 
 import com.beetrootmonkey.myutils.block.ModBlocks;
 import com.beetrootmonkey.myutils.client.ModTab;
+import com.beetrootmonkey.myutils.config.ConfigLoader;
+import com.beetrootmonkey.myutils.crafting.ModRecipes;
+import com.beetrootmonkey.myutils.drops.DropHandler;
 import com.beetrootmonkey.myutils.item.ModItems;
+import com.beetrootmonkey.myutils.item.fuel.FuelHandler;
 import com.beetrootmonkey.myutils.proxy.CommonProxy;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MC_VERSIONS)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MC_VERSIONS, guiFactory = Reference.GUI_FACTORY)
 public class MyUtils {
 
 	public static final ModTab creativeTab = new ModTab();
@@ -24,14 +30,16 @@ public class MyUtils {
 
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		System.out.println(Reference.MOD_NAME + " is loading!");
+		ConfigLoader.load(event);
 		ModItems.init();
 		ModBlocks.init();
+		MinecraftForge.EVENT_BUS.register(new DropHandler());
+		GameRegistry.registerFuelHandler(new FuelHandler());
 	}
 
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
-
+		ModRecipes.addRecipes();
 	}
 
 	@Mod.EventHandler
